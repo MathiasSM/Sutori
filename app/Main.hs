7aaade7cd4db02406ee1d2671af597431931bbe6
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-07-22 20:19:05 -0400

    hlinted Main

--- app/Main.hs
+++ app/Main.hs
@@ -12,25 +12,25 @@ import Sutori.Monad
 
 main = do
   args <- getArgs
-  s <- readFile $ (args !! 1)
+  s <- readFile (args !! 1)
   let sr = runAlexScan s
   when (isLeft sr) $ error $ extractL sr
   let ws =  filter isInvalid (extractR sr)
-      ls = (extractR sr)
-  when (length ws /= 0) $ do putStrLn "Error Lexicografico, Alex isn't Happy:(\nTokens Invalidos:"
-                             mapM_ print ws
-  case (head args) of
+      ls = extractR sr
+  when (not null ws) $ do putStrLn "Error Lexicografico, Alex isn't Happy:(\nTokens Invalidos:"
+                          mapM_ print ws
+  case head args of
       "-l" -> mapM_ (putStrLn. printToken) ls
       "-p" -> do let (ast,_,_) = getTuple (calc ls) emptyState
                  printSource 0 ast
-      otherwise -> do let tuple = getTuple (calc ls) emptyState
-                      case tuple of
-                        (ast,sym,OurLog "") -> do putStrLn "AST: "
-                                                  printSource 0 ast
-                                                  putStrLn "SYMTABLE: "
-                                                  printSymTable sym
-                                                  putStrLn "0 Errors."
-                        otherwise -> printLog tuple
-  where printLog (_,_,log) = print $ log
+      _ -> do let tuple = getTuple (calc ls) emptyState
+              case tuple of
+                (ast,sym,OurLog "") -> do putStrLn "AST: "
+                                          printSource 0 ast
+                                          putStrLn "SYMTABLE: "
+                                          printSymTable sym
+                                          putStrLn "0 Errors."
+                _ -> printLog tuple
+  where printLog (_,_,log) = print log
         extractL (Left e) = e
         extractR (Right l) = l
