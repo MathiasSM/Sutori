c8ec38e691c2ee2f095d06a98639b2d75a2b7af6
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-09-09 16:54:24 -0400

    Better command-line options handling (WIP)

--- app/Main.hs
+++ app/Main.hs
@@ -2,35 +2,40 @@ module Main where
 
 import System.Environment
 import Control.Monad
-import Data.Maybe
 import Data.Either
 
-import Sutori.Lexer
-import Sutori.Parser
-import Sutori.AST
-import Sutori.Monad
+import Sutori.Options
+-- import Sutori.Lexer
+-- import Sutori.Parser
+-- import Sutori.AST
+-- import Sutori.Monad
 
+
+
+-- Sutori runner
 main = do
-  args <- getArgs
-  s <- readFile (args !! 1)
-  let sr = runAlexScan s
-  when (isLeft sr) $ error $ extractL sr
-  let ws =  filter isInvalid (extractR sr)
-      ls = extractR sr
-  when (not null ws) $ do putStrLn "Error Lexicografico, Alex isn't Happy:(\nTokens Invalidos:"
-                          mapM_ print ws
-  case head args of
-      "-l" -> mapM_ (putStrLn. printToken) ls
-      "-p" -> do let (ast,_,_) = getTuple (calc ls) emptyState
-                 printSource 0 ast
-      _ -> do let tuple = getTuple (calc ls) emptyState
-              case tuple of
-                (ast,sym,OurLog "") -> do putStrLn "AST: "
-                                          printSource 0 ast
-                                          putStrLn "SYMTABLE: "
-                                          printSymTable sym
-                                          putStrLn "0 Errors."
-                _ -> printLog tuple
-  where printLog (_,_,log) = print log
-        extractL (Left e) = e
-        extractR (Right l) = l
+  argv <- getArgs
+  (options, files) <- handleFlags argv
+  printLn (show options)
+  --
+  -- let sr = runAlexScan s
+  -- when (isLeft sr) $ error $ extractL sr
+  -- let ws =  filter isInvalid (extractR sr)
+  --     ls = extractR sr
+  -- when (not null ws) $ do putStrLn "Error Lexicografico, Alex isn't Happy:(\nTokens Invalidos:"
+  --                         mapM_ print ws
+  -- case head args of
+  --     "-l" -> mapM_ (putStrLn. printToken) ls
+  --     "-p" -> do let (ast,_,_) = getTuple (calc ls) emptyState
+  --                printSource 0 ast
+  --     _ -> do let tuple = getTuple (calc ls) emptyState
+  --             case tuple of
+  --               (ast,sym,OurLog "") -> do putStrLn "AST: "
+  --                                         printSource 0 ast
+  --                                         putStrLn "SYMTABLE: "
+  --                                         printSymTable sym
+  --                                         putStrLn "0 Errors."
+  --               _ -> printLog tuple
+  -- where printLog (_,_,log) = print log
+  --       extractL (Left e) = e
+  --       extractR (Right l) = l
