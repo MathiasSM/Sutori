244080b376433c1b753a7c87f57f26cf986a6b12
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-09-16 17:24:06 -0400

    Fixed table

--- src/Sutori/SymTable.hs
+++ src/Sutori/SymTable.hs
@@ -4,7 +4,7 @@ import qualified Data.Map as Map
 
 import Sutori.Logger(SutShow(showSut), SutLog(SutLogNode, SutLogLeave))
 import Sutori.AST(SutID, SutBlock)
-import Sutori.Types
+import Sutori.Types(SutType)
 
 
 
@@ -40,9 +40,9 @@ instance SutShow SutParamKind where
 
 
 -- A SutSymOther is either a Function definition AST, the parameter kind, the type definition or nothing
-data SutSymOther = SymAST {getParams :: [SutParam], getBlock :: SutBlock }
-                 | SymParamKind {getParamKind :: SutParamKind}
-                 | SymTypeDef {typeOf :: SutType}
+data SutSymOther = SymAST       { otherASTParams :: [SutParam], otherASTBlock :: SutBlock }
+                 | SymParamKind { otherParamKind :: SutParamKind }
+                 | SymTypeDef   { otherTypeDef   :: SutType }
                  | SymNothing
 
 instance SutShow SutSymOther where
@@ -56,78 +56,86 @@ instance SutShow SutSymOther where
 
 -- A SutSymbol is made of a Token, a Category, a Scope, a Type, and perhaps other information
 data SutSymbol = SutSymbol {
-  getSymID    :: SutID,
-  getCat      :: SutSymCategory,
-  getScope    :: Scope,
-  getType     :: SutType,
-  getOther    :: SutSymOther
+  symID    :: SutID,
+  symCat   :: SutSymCategory,
+  symScope :: Scope,
+  symType  :: SutType,
+  symOther :: SutSymOther
 }
 
 instance SutShow SutSymbol where
-  showSut s = let etype = SutLogNode "Type:" [showSut (getType s)]
-                  cat = showSut $ getCat s
-                  scope = SutLogLeave ("Scope: " ++ show (getScope s))
-                  other = showSut (getOther s)
-               in SutLogNode ("Symbol: " ++ getSymID s) [cat, etype, scope, other]
+  showSut s = let etype = SutLogNode "Type:" [showSut (symType s)]
+                  cat   = showSut $ symCat s
+                  scope = SutLogLeave ("Scope: " ++ show (symScope s))
+                  other = showSut (symOther s)
+               in SutLogNode ("Symbol: " ++ symID s) [cat, etype, scope, other]
 
 
 -- A SutParam has a kind, a type and a token representing
 data SutParam = SutParam {
-  getKind      :: SutParamKind,
-  getParamType :: SutType,
-  getParamID   :: SutID
+  paramKind :: SutParamKind,
+  paramType :: SutType,
+  paramID   :: SutID
 }
 
 instance SutShow SutParam where
-  showSut p = let kind = showSut (getKind p)
-                  etype = SutLogNode "Type:" [showSut (getParamType p)]
-               in SutLogNode ("Parameter: " ++ getParamID p) [etype, kind]
+  showSut p = let kind  = showSut (paramKind p)
+                  etype = SutLogNode "Type:" [showSut (paramType p)]
+               in SutLogNode ("Parameter: " ++ paramID p) [etype, kind]
 
 
 -- A SymTable matches a SutID (symbol name) to a list of symbols
 type SymTable = Map.Map SutID [SutSymbol]
 
 
---
--- -- Actions on the SymTable
--- -- ===============================================================================================
---
--- -- Insertion
--- -- ------------------------------------------------------------------------------------------------
--- -- Inserts? a list of IDs witha given function
--- insertWith :: SymTable -> [a] -> (SymMap -> a -> SymMap) -> SymTable
--- insertWith table ids f = SymTable $ foldl f (getHash table) ids
---
--- -- Inserts into the table the given constructed symbol
--- insertSymbol :: SymMap -> SutSymbol -> SymMap
--- insertSymbol hash s = let id = getSymbolID s
---                           syms = Map.findWithDefault [] id hash
---                        in Map.insert id (s:syms) hash
---
--- -- Inserts new symbols into the table (from a list of tokens)
--- insert :: SymTable -> Scope -> SutSymCategory -> SutType -> SutSymOther -> [SutToken] -> SymTable
--- insert table curScope category t o ids = insertWith table ids f
---   where f hash tk = let newSymbol = SutSymbol tk category curScope t o
---                      in insertSymbol hash newSymbol
---
--- -- Inserts new symbols into the table (from a list of params)
--- insertParams :: SymTable -> Scope -> SutSymCategory -> [SutParam] -> SymTable
--- insertParams table curScope category ps = insertWith table ps f
---   where f hash (SutParam k t tk) = let newSymbol = SutSymbol tk category curScope t (ParamKind k)
---                                     in insertSymbol hash newSymbol
---
---
--- -- Lookup
--- ---------------------------------------------------------------------------------------------------
--- -- Search for all symbols that match a SutID
--- lookupID :: SymTable -> SutID -> [SutSymbol]
--- lookupID table id = Map.findWithDefault [] id $ getHash table
---
--- -- Search for all symbols that match a token in a given scope
--- lookupInScope :: SymTable -> Scope -> SutToken -> [SutSymbol]
--- lookupInScope table scope tk = filter isHere $ lookupID table $ getTokenID tk
---   where isHere = (== scope) . getScope
---
--- -- Makes several token searches in the same scope
--- lookupsInScope :: SymTable -> Scope -> [SutToken] -> [[SutSymbol]]
--- lookupsInScope table scope = map (lookupInScope table scope)
+
+-- Actions on the SymTable
+-- ===============================================================================================
+
+-- Insertion
+-- ------------------------------------------------------------------------------------------------
+-- Folds a list of IDs with a given function over the table
+insertWith :: SymTable -> [a] -> (SymTable -> a -> SymTable) -> SymTable
+insertWith table ids f = foldl f table ids
+
+-- Inserts symbol into the table the given constructed symbol
+insertSymbol :: SymTable -> SutSymbol -> SymTable
+insertSymbol table s = let id   = symID s
+                           syms = lookupID table id
+                        in Map.insert id (s:syms) table
+
+-- Inserts new symbols into the table (from a list of IDs)
+insert :: SymTable -> Scope -> SutSymCategory -> SutType -> SutSymOther -> [SutID] -> SymTable
+insert table scope cat t o ids = insertWith table ids insertID
+  where insertID table id = let newSymbol = SutSymbol { symID    = id,
+                                                        symCat   = cat,
+                                                        symScope = scope,
+                                                        symType  = t,
+                                                        symOther = o }
+                             in insertSymbol table newSymbol
+
+-- Inserts new symbols into the table (from a list of params)
+insertParams :: SymTable -> Scope -> SutSymCategory -> [SutParam] -> SymTable
+insertParams table scope cat ps = insertWith table ps insertParam
+  where insertParam table' p = let newSymbol = SutSymbol { symID    = paramID p,
+                                                           symCat   = cat, -- TODO: Isn't this always CatParameter?
+                                                           symScope = scope,
+                                                           symType  = paramType p,
+                                                           symOther = SymParamKind $ paramKind p }
+                                in insertSymbol table' newSymbol
+
+
+-- Lookup
+---------------------------------------------------------------------------------------------------
+-- Search for all symbols that match a SutID
+lookupID :: SymTable -> SutID -> [SutSymbol]
+lookupID table id = Map.findWithDefault [] id table
+
+-- Search for all symbols that match a token in a given scope
+lookupInScope :: SymTable -> Scope -> SutID -> [SutSymbol]
+lookupInScope table scope id = filter isHere $ lookupID table id
+  where isHere = (== scope) . symScope
+
+-- Makes several token searches in the same scope
+lookupsInScope :: SymTable -> Scope -> [SutID] -> [[SutSymbol]]
+lookupsInScope table scope = map (lookupInScope table scope)
