7ebd953f970dc82c2f9ca55882d81c326d95e61b
Mathias San Miguel <mathiassanmiguel@gmail.com>
2018-10-21 16:31:18 -0400

    Fix ignore stupidity

---
+++ src/Sutori/Lexer.hs
@@ -0,0 +1,17 @@
+{-|
+Description : API for the alex-generated lexer for Sutori language tokens
+-}
+module Sutori.Lexer
+( lexerScanClean -- We only export the clean version (has lexical error handling)
+, runLexer       -- Runs the lexer with the given action (parsing?)
+, runLexerScan   -- Runs the lexer by itself (with the usual scan loop)
+, lexwrap        -- Wrapper around scan used by happy
+, SutToken(..)
+, initialPosn
+, SutPosn
+) where
+
+import Sutori.Lexer.Lexer
+import Sutori.Lexer.Posn
+import Sutori.Lexer.Tokens
+import Sutori.Lexer.Logger
---
+++ src/Sutori/Parser.hs
@@ -0,0 +1,10 @@
+{-|
+Description : Defines API for Happy-generated parser for Sutori language
+-}
+module Sutori.Parser
+( parseModule
+, parseExpression
+, parseType
+) where
+
+import Sutori.Parser.Parser
